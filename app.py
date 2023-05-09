03bd26e4210a6c5419b84cd53cb6a57e9ef6e90a
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2023-05-09 15:29:49 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import requests
+
+request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return request.json()
