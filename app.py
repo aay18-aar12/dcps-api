4d2e6cbfb0a4583d4ac32cde3c493029bf8c815b
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2023-05-26 00:12:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def hello_world():
 
 @app.route('/school/{code}')
 def retSchool(code: int):
-    request = [x for x in request if x['SCHOOL_CODE'] == code]
+    request = [x for x in request.json() if x['SCHOOL_CODE'] == code]
     return request.json()
