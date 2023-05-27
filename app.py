40dbe928fc9e99fef05223d3f392b4be16ebe6a9
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2023-05-27 14:22:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,22 @@ app = Flask(__name__)
 def hello_world():
     return request.json()
 
-@app.route('/school')
-def retSchool():
-    return request.json()
+@app.route('/code/{code}')
+def retCode(code: int):
+    final_set = {}
+    bo = request.json()
+
+    def my_filtering_function(pair):
+        key, value = pair
+        if key == 'features':
+            for i in value:
+                if i.get('attributes').get('SCHOOL_CODE') == code:
+                    final_set.update({key: [final_set.get('features'), i]})
+                
+        else:
+            return False
+        
+
+    filtered_data= dict(filter(my_filtering_function, bo.items()))
+
+    return final_set
