665011e204192f10583e09746bb2ac30ffdbd30e
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2023-06-11 23:53:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,28 +9,10 @@ app = Flask(__name__)
 def hello_world():
     return request.json()
 
-@app.route('/code/<int:code>')
-def retCode(code: int):
-    final_set = {}
-    bo = request.json()
-    yo = []
-
-    def my_filtering_function(pair):
-        key, value = pair
-        if key == 'features':
-            for i in value:
-                if i.get('attributes').get('SCHOOL_CODE') == code:
-                    yo.append(i)
-                    final_set.update({key: yo})
-                
-        else:
-            return False
-        
-
-    filtered_data= dict(filter(my_filtering_function, bo.items()))
-
-    return final_set
-
+@app.route('/code/<string:code>')
+def retCode(code):
+    codeRequest = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=%20(SCHOOL_CODE%20%3D%20' + code + ')%20&outFields=*&outSR=4326&f=json')
+    return (codeRequest.json())
 
 @app.route('/name/<string:name>')
 def retName(name):
