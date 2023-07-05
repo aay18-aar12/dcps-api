373b2e5154ea24d5756ed98dae3c960e7810e525
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2023-07-05 16:47:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import requests
 import math
 
-request2 = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=1%3D1&outFields=*&outSR=4326&returnCountOnly=true&f=json')
+request2 = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=SCHOOL_YEAR%20%3D%20%272015-16%27&outFields=*&outSR=4326&returnCountOnly=true&f=json')
 
 countHolder = request2.json()
 
@@ -17,23 +17,23 @@ app = Flask(__name__)
 def hello_world():
     final_set = {}
     yo = []
-
-    def my_filtering_function(pair):
-        key, value = pair
-        if key == 'features':
-            for i in value:
-                yo.append(i)
-                final_set.update({key: yo})
+    for i in range(globCount):
+        ko = str(i*1000)
+        request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=SCHOOL_YEAR%20%3D%20%272015-16%27&outFields=*&outSR=4326&resultOffset='+ko+'&f=json')
+        bo = request.json()
+        def my_filtering_function(pair):
+            key, value = pair
+            if key == 'features':
+                for i in value:
+                    yo.append(i)
+                    final_set.update({key: yo})
 
 
                 
-        else:
-            return False
+            else:
+                return False
         
-    for i in range(globCount):
-        ko = str(i*1000)
-        request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=1%3D1&outFields=*&outSR=4326&resultOffset='+ko+'&f=json')
-        bo = request.json()
+
         filtered_data = dict(filter(my_filtering_function, bo.items()))
 
     return final_set
