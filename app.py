0e6bc7cf21c528f4f1618db25c9eb5b407655318
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2023-07-06 02:08:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,3 +39,40 @@ def hello_world(school_code):
         filtered_data = dict(filter(my_filtering_function, bo.items()))
 
     return final_set
+
+
+
+
+@app.route('/subject/<string:school_code>/<string:subject>')
+def retCode(school_code, subject):
+
+    request2 = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=%20(SCHOOL_CODE%20%3D%20'+school_code+')%20&outFields=*&outSR=4326&returnCountOnly=true&f=json')
+
+    countHolder = request2.json()
+
+    count = countHolder.get('count')
+
+    globCount = (math.trunc(count/1000))+1
+    final_set = {}
+    yo = []
+
+    def my_filtering_function(pair):
+        key, value = pair
+        if key == 'features':
+            for i in value:
+                if i.get('attributes').get('subject') == subject:
+                    yo.append(i)
+                    final_set.update({key: yo})
+
+
+                
+        else:
+            return False
+        
+    for i in range(globCount):
+        ko = str(i*1000)
+        request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Education_WebMercator/MapServer/23/query?where=%20(SCHOOL_CODE%20%3D%20'+school_code+')%20&outFields=*&outSR=4326&resultOffset='+ko+'&f=json')
+        bo = request.json()
+        filtered_data = dict(filter(my_filtering_function, bo.items()))
+
+    return final_set
